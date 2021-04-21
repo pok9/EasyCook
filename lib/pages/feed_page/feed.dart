6b20862cb4d7ce189a92e0251bd7d0f43807d485
pok9 <pokhappy1999@gmail.com>
2021-04-21 10:49:55 +0700

    update_feed_search

--- lib/pages/feed_page/feed.dart
+++ lib/pages/feed_page/feed.dart
@@ -1,21 +1,45 @@
-import 'package:flutter/cupertino.dart';
+
 import 'package:flutter/material.dart';
 
-class FeedPage extends StatefulWidget {
-  // FeedPage({Key key}) : super(key: key);
+class FeedPage extends StatelessWidget {
+  const FeedPage({Key key}) : super(key: key);
 
-  @override
-  _FeedPageState createState() => _FeedPageState();
-}
-
-class _FeedPageState extends State<FeedPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(40.0),
-        child: AppBar(
-          title: Text('Easy Cook'),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 3.0,bottom: 10),
+                child: Text("Explore",style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),),
+              ),
+              Container(
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(30),
+                  border: Border.all(color: Colors.grey)
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: "Find a food or Restaur",
+                      prefixIcon: Icon(Icons.search,color: Colors.indigo,),
+                      suffixIcon: Icon(Icons.add_road_rounded,color: Colors.grey,),
+                      focusedBorder: InputBorder.none,
+                      enabledBorder: InputBorder.none,
+                      errorBorder: InputBorder.none,
+                      disabledBorder: InputBorder.none
+                    ),
+                  ),
+                )),
+            ],
+          ),
         ),
       ),
     );
