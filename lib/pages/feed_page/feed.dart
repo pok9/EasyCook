a2b6d85125c91fbd2664d41b458becbc925b80f7
pok9 <pokhappy1999@gmail.com>
2021-04-22 19:56:59 +0700

    update_image_feed

--- lib/pages/feed_page/feed.dart
+++ lib/pages/feed_page/feed.dart
@@ -5,8 +5,144 @@ class FeedPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var deviceSize = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Color(0xFFf3f5f9),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(40.0),
+        child: AppBar(
+          title: Text('Easy Cook'),
+        ),
+      ),
+      drawer: Container(
+        width: deviceSize.width - 45,
+        child: Drawer(
+          child: ListView(
+            children: [
+              GestureDetector(
+                onTap: () {},
+                child: DrawerHeader(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: new NetworkImage(
+                          "https://img.freepik.com/free-vector/blue-copy-space-digital-background_23-2148821698.jpg?size=626&ext=jpg"),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          TextButton(
+                              onPressed: () {
+                                Navigator.pushNamed(context, '/login-page');
+                              },
+                              child: Text(
+                                'เข้าสู่ระบบ',
+                              ),
+                              style: ButtonStyle(
+                                  side: MaterialStateProperty.all(BorderSide(
+                                      width: 2, color: Colors.white)),
+                                  foregroundColor:
+                                      MaterialStateProperty.all(Colors.white),
+                                  padding: MaterialStateProperty.all(
+                                      EdgeInsets.symmetric(
+                                          vertical: 10, horizontal: 50)),
+                                  textStyle: MaterialStateProperty.all(
+                                      TextStyle(fontSize: 15)))),
+                          TextButton(
+                              onPressed: () {
+                                Navigator.pushNamed(context, '/register-page');
+                              },
+                              child: Text(
+                                'สมัครสมาชิก',
+                              ),
+                              style: ButtonStyle(
+                                  side: MaterialStateProperty.all(BorderSide(
+                                      width: 2, color: Colors.white)),
+                                  foregroundColor:
+                                      MaterialStateProperty.all(Colors.white),
+                                  padding: MaterialStateProperty.all(
+                                      EdgeInsets.symmetric(
+                                          vertical: 10, horizontal: 43)),
+                                  textStyle: MaterialStateProperty.all(
+                                      TextStyle(fontSize: 15)))),
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(15, 0, 0, 0),
+                        child: Column(
+                          children: [
+                            Row(),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.folder,
+                  color: Colors.cyan,
+                  size: 30,
+                ),
+                title: Text('สูตรที่ซื้อ',
+                    style: TextStyle(
+                        fontWeight: FontWeight.w300,
+                        fontSize: 23,
+                        color: Colors.black)),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.notifications,
+                  color: Colors.cyan,
+                  size: 30,
+                ),
+                title: Text('การแจ้งเตือน',
+                    style: TextStyle(
+                        fontWeight: FontWeight.w300,
+                        fontSize: 23,
+                        color: Colors.black)),
+                onTap: () {},
+              ),
+              Divider(
+                thickness: 0.5,
+                color: Colors.grey,
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.settings,
+                  color: Colors.cyan,
+                  size: 30,
+                ),
+                title: Text('ตั้งค่า',
+                    style: TextStyle(
+                        fontWeight: FontWeight.w300,
+                        fontSize: 23,
+                        color: Colors.black)),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.exit_to_app,
+                  color: Colors.cyan,
+                  size: 30,
+                ),
+                title: Text('ออกจากระบบ',
+                    style: TextStyle(
+                        fontWeight: FontWeight.w300,
+                        fontSize: 23,
+                        color: Colors.black)),
+                onTap: () {},
+              ),
+            ],
+          ),
+        ),
+      ),
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(8.0),
@@ -15,44 +151,44 @@ class FeedPage extends StatelessWidget {
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
+                  // Padding(
+                  //   padding: const EdgeInsets.only(left: 3.0, bottom: 10),
+                  //   child: Text(
+                  //     "Easy Cook",
+                  //     style:
+                  //         TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                  //   ),
+                  // ),
+                  // Container(
+                  //     height: 50,
+                  //     decoration: BoxDecoration(
+                  //         color: Colors.white,
+                  //         borderRadius: BorderRadius.circular(30),
+                  //         border: Border.all(color: Colors.grey)),
+                  //     child: Padding(
+                  //       padding: const EdgeInsets.all(8.0),
+                  //       child: TextField(
+                  //         decoration: InputDecoration(
+                  //             hintText: "Find a food or Restaur",
+                  //             prefixIcon: Icon(
+                  //               Icons.search,
+                  //               color: Colors.indigo,
+                  //             ),
+                  //             suffixIcon: Icon(
+                  //               Icons.add_road_rounded,
+                  //               color: Colors.grey,
+                  //             ),
+                  //             focusedBorder: InputBorder.none,
+                  //             enabledBorder: InputBorder.none,
+                  //             errorBorder: InputBorder.none,
+                  //             disabledBorder: InputBorder.none),
+                  //       ),
+                  //     )),
+                  // SizedBox(
+                  //   height: 25,
+                  // ),
                   Padding(
-                    padding: const EdgeInsets.only(left: 3.0, bottom: 10),
-                    child: Text(
-                      "Easy Cook",
-                      style:
-                          TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                  Container(
-                      height: 50,
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(30),
-                          border: Border.all(color: Colors.grey)),
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: TextField(
-                          decoration: InputDecoration(
-                              hintText: "Find a food or Restaur",
-                              prefixIcon: Icon(
-                                Icons.search,
-                                color: Colors.indigo,
-                              ),
-                              suffixIcon: Icon(
-                                Icons.add_road_rounded,
-                                color: Colors.grey,
-                              ),
-                              focusedBorder: InputBorder.none,
-                              enabledBorder: InputBorder.none,
-                              errorBorder: InputBorder.none,
-                              disabledBorder: InputBorder.none),
-                        ),
-                      )),
-                  SizedBox(
-                    height: 25,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 8,right: 8),
+                    padding: const EdgeInsets.only(left: 8, right: 8),
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
@@ -61,7 +197,7 @@ class FeedPage extends StatelessWidget {
                           style: TextStyle(
                               fontSize: 20, fontWeight: FontWeight.bold),
                         ),
-                        Icon(Icons.arrow_forward_rounded,color: Colors.indigo)
+                        Icon(Icons.arrow_forward_rounded, color: Colors.indigo)
                         // Text(
                         //   "ดูทั้งหมด",
                         //   style: TextStyle(
@@ -71,7 +207,7 @@ class FeedPage extends StatelessWidget {
                     ),
                   ),
                   Container(
-                      height: 200,
+                      height: 240,
                       child: ListView.builder(
                           scrollDirection: Axis.horizontal,
                           itemCount: 5,
@@ -80,7 +216,7 @@ class FeedPage extends StatelessWidget {
                           })),
 
                   Padding(
-                    padding: const EdgeInsets.only(left: 8,right: 8),
+                    padding: const EdgeInsets.only(left: 8, right: 8),
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
@@ -89,7 +225,7 @@ class FeedPage extends StatelessWidget {
                           style: TextStyle(
                               fontSize: 20, fontWeight: FontWeight.bold),
                         ),
-                        Icon(Icons.arrow_forward_rounded,color: Colors.indigo)
+                        Icon(Icons.arrow_forward_rounded, color: Colors.indigo)
                         // Text(
                         //   "ดูทั้งหมด",
                         //   style: TextStyle(
@@ -99,7 +235,7 @@ class FeedPage extends StatelessWidget {
                     ),
                   ),
                   Container(
-                      height: 200,
+                      height: 240,
                       child: ListView.builder(
                           scrollDirection: Axis.horizontal,
                           itemCount: 5,
@@ -114,7 +250,7 @@ class FeedPage extends StatelessWidget {
                         style: TextStyle(
                             fontSize: 20, fontWeight: FontWeight.bold),
                       ),
-                      Icon(Icons.arrow_forward_rounded,color: Colors.indigo)
+                      Icon(Icons.arrow_forward_rounded, color: Colors.indigo)
                       // Text(
                       //   "ดูทั้งหมด",
                       //   style: TextStyle(
@@ -131,7 +267,7 @@ class FeedPage extends StatelessWidget {
                             return _foodCardSlim_1(context);
                           })),
                 ],
-              ), 
+              ),
             ],
           ),
         ),
@@ -143,11 +279,9 @@ class FeedPage extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.fromLTRB(4, 8, 4, 8),
       child: Card(
-         
         child: Container(
           color: Colors.white,
           width: 200,
-          
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
@@ -170,6 +304,26 @@ class FeedPage extends StatelessWidget {
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
+                        // Container(
+                        //   height: 40.0,
+                        //   width: 40.0,
+                        //   decoration: new BoxDecoration(
+                        //       shape: BoxShape.circle,
+                        //       image: new DecorationImage(
+                        //           fit: BoxFit.fill,
+                        //           image: new NetworkImage(
+                        //               "https://images.unsplash.com/photo-1484723091739-30a097e8f929?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80"))),
+                        // ),
+                        Container(
+                          height: 40.0,
+                          width: 40.0,
+                          decoration: new BoxDecoration(
+                              shape: BoxShape.circle,
+                              image: new DecorationImage(
+                                  fit: BoxFit.fill,
+                                  image: new NetworkImage(
+                                      "https://images.unsplash.com/photo-1484723091739-30a097e8f929?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80"))),
+                        ),
                         Text(
                           "ต้ำยำกุ้ง",
                           style: TextStyle(
